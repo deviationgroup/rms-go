46c46b2622df76d9f4d7d513d62fbbc613aeb472
ah4rtaylor <tnichols@ah4r.com>
2017-08-07 16:13:28 -0700

    master

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:rms-go.database.windows.net,1433; Database = webapp-sql-group-db", "rmsadmin", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "rmsadmin@rms-go", "pwd" => "{your_password_here}", "Database" => "webapp-sql-group-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:rms-go.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+?>
